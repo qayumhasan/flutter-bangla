3e726a1986cc2efded48be852de37149191f7e83
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-12-09 14:16:00 +0600

    Create card.dart

---
+++ card.dart
@@ -0,0 +1,9 @@
+child: Card(
+                color: Colors.green,
+                elevation: 50,
+                shadowColor: Colors.deepOrange,
+                child: Container(
+                  height: 200,
+                  width: 250,
+                ),
+              ),
