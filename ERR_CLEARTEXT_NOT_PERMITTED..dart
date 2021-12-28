79602b4069d946ace6763d336ae3ee4949ec8911
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-12-28 15:34:46 +0600

    Create ERR_CLEARTEXT_NOT_PERMITTED..dart

---
+++ ERR_CLEARTEXT_NOT_PERMITTED..dart
@@ -0,0 +1,7 @@
+// net::ERR_CLEARTEXT_NOT_PERMITTED.
+// path : android/app/src/main/AndroidManifest.xml
+<application
+    android:name="io.flutter.app.FlutterApplication"
+    android:label="tangerine_ui"
+    android:icon="@mipmap/ic_launcher"
+    android:usesCleartextTraffic="true">
